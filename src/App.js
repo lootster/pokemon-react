4ca91a61a9042c84ac1fa5ccd87772acef0cfbfb
lootster <sheldonkhlim@gmail.com>
2018-10-06 15:41:24 +0800

    Render color based on the pokemon type

--- src/App.js
+++ src/App.js
@@ -15,22 +15,33 @@ class App extends Component {
   }
 
   handleChange(event) {
-    this.setState({value: event.target.value});
+    this.setState({value: event.target.value.toLowerCase()});
   }
   
   render() {
+
+    const filteredData = this.state.pokemon.filter(element => element.name.toLowerCase().includes(this.state.value))
+
+    const display = filteredData.length > 0 && 
+      filteredData.map(element => {
+        return <Card pokemon={element}/>
+      })
+
+    const notDisplay = filteredData.length === 0 && 
+     <p>No pokemon available</p>
+    
+
+    console.log(filteredData)
+
     return (
       <div className="wrapper">
         <div className="textInput">
-          <input type="text" placeholder="Search your pokedex!" value={this.state.value} onChange={this.handleChange} />
+          <input type="text" placeholder="Search your pokedex!" value={this.state.value} onChange={this.handleChange.bind(this)} />
         </div>
         
         <div className="list">
-        {
-          this.state.pokemon.map(element => {
-            return <Card pokemon={element} /> 
-          })
-        }
+        {display}
+        {notDisplay}
         </div>
       </div>
     );
--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,16 @@
 import React from "react";
 
 const Card = props => {
-  const {pokemon: {name, type, price, image}} = props;
+  const {
+    pokemon: { name, type, price, image }
+  } = props;
   return (
-    <div className="listItem">
-      <img src= {image} alt="image"/>
+    <div className="listItem" id={type}>
+      <img src={image} alt="image" />
       <h3 className="listItem__name">{name}</h3>
-      <h3 className="listItem__name">Pokemon Type: {type.charAt(0).toUpperCase() + type.slice(1)}</h3>
+      <h3 className="listItem__name">
+        Pokemon Type: {type.charAt(0).toUpperCase() + type.slice(1)}
+      </h3>
       <div className="listItem__price">
         <div className="listItem__priceTxt">{price}</div>
       </div>
